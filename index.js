7fa0958722bf129665b0763c4bc0575f59478bbc
Anthony Casson <anthonycasson@Anthonys-MacBook-Pro.local>
2017-08-07 23:16:44 -0700

    Get simple server started, basic dependencies

--- index.js
+++
@@ -1,7 +0,0 @@
-//core modules
-import express from "express";
-//routes
-
-const app = express();
-//environment
-const port = process.env.PORT || 3000;
--- server.js
+++ server.js
@@ -1,4 +1,11 @@
-//Babel/register for ES6
-require("babel/register");
-//Index.js
-require("./index");
+//core modules
+const express = require('express');
+//routes
+//environment
+const port = process.env.PORT || 3000;
+
+const app = express();
+
+const server = app.listen(port, () => {
+    console.log(`Server started on port ${port}`);
+});
